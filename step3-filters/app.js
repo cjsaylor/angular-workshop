11bef00521455629528933def30e8bb6f9c04708
Chris Saylor <cjsaylor@gmail.com>
2015-01-10 19:12:50 -0500

    Add view filter for capitalizing words.
    First part of step3 filters.

---
+++ step3-filters/app.js
@@ -0,0 +1,71 @@
+(function() {
+	"use strict";
+    var app = angular.module('workshop', []);
+
+    var setLoadingDone = function() {
+        this.loading = false;
+    };
+
+    app.controller('Catalog', ['$scope', 'catalog', function($scope, catalog) {
+        $scope.loading = true;
+        catalog.retrieve()
+            .then(function(items) {
+                $scope.items = items;
+            })
+            .finally(setLoadingDone.bind($scope));
+    }]);
+
+    app.controller('Filter', ['$scope', 'catalog', function($scope, catalog) {
+        catalog.availableColors()
+            .then(function(colors) {
+                $scope.colors = colors;
+            });
+    }]);
+
+    // View filter that capitalizes words
+    // Use: {{ sentence | capitalize }}
+    app.filter('capitalize', function() {
+        return function(input) {
+            return input.split(' ').map(function(word) {
+                return word[0].toUpperCase() + word.substr(1);
+            }).join(' ');
+        };
+    });
+
+    app.service('catalog', ['$http', '$q', function($http, $q) {
+        var catalogCache;
+        var retrieve = function() {
+            var deferred = $q.defer();
+            if (catalogCache) {
+                deferred.resolve(catalogCache);
+            } else {
+                $http.get('../data/catalog.json')
+                    .success(deferred.resolve)
+                    .error(deferred.reject);
+            }
+            return deferred.promise;
+        };
+
+        var availableColors = function() {
+            return retrieve()
+                // Map the colors
+                .then(function(items) {
+                    return items.map(function(item) {
+                        return item.color;
+                    });
+                })
+                // Filter out duplicates
+                .then(function(colors) {
+                    var seen = {};
+                    return colors.filter(function(color) {
+                        return seen[color] ? false : seen[color] = true;
+                    });
+                });
+        }
+        return {
+            retrieve: retrieve,
+            availableColors: availableColors
+        };
+    }]);
+
+}());
